ca20bf05198598f68e022b6673a60244e29ea583
zicozydailva <zicozydasilva70@gmail.com>
2023-11-01 02:56:56 +0100

    newer-version-recap

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>FULLSTACK DEVELOPER</h1>
+            <Link href='/'>Dashboard</Link>
+            <Link href='/tickets'>Tickets</Link>
+        </nav>
+    )
+}
